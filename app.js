1031f63088c0dd7f237c903a25042aee42242d03
jawwadmunir788 <jawadbhatti0465@gmail.com>
2025-11-24 08:51:12 +0500

    Enhance UI Fix Error

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const routes = require("./src/routes");
 const app = express();
 
 app.use(express.json());
+
+// 🔥 FIX: Serve static files (YOUR UI)
+app.use(express.static("public"));
+
 app.use("/", routes);
 
 module.exports = app;
